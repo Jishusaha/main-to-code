0f29747db4a35813a72b8204b978c57e17e2ad70
Jishusaha <sahajishu669@gmail.com>
2026-07-28 02:30:00 +0530

    Time: 0 ms (100%), Space: 12.4 MB (20.41%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        a=int(num1)
+        b=int(num2)
+        mul=a*b
+        num=str(mul)
+        return num
+
+        
+        
